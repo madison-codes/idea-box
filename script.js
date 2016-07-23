3fe8d6be20fba7a1ce97024fe72c1a69937b11f8
Madison Kerndt <madison.kerndt@gmail.com>
2016-07-23 16:08:24 -0600

    undo changes

--- script.js
+++ script.js
@@ -1,80 +1,20 @@
+var ideaStore = new IdeaLocalStorage();
+
 function clearInputFields() {
   $('.title-input-field').val('');
   $('.body-input-field').val('');
 }
 
-// // Constructor: Allows to make n amount of instances (object)
+// function IdeaLocalStorage() {
+//   this.storeKey = ideas;
+//   this.ideaStore = [];
+// }
+//
+
+// *mentor
 function IdeaLocalStorage() {
   this.storeKey = 'ideas';
-  this.ideas = [];
 }
-
-IdeaLocalStorage.prototype.getIdeas = function() {
-  return JSON.parse(window.localStorage.getItem(this.storeKey)) || [];
-}
-
-IdeaLocalStorage.prototype.addIdea = function(idea) {
-  var ideas = this.getIdeas();
-  ideas.unshift(idea);
-  window.localStorage.setItem(this.storeKey, JSON.stringify(ideas));
-}
-
-IdeaLocalStorage.prototype.removeIdea = function(e) {
-  // the id of the idea that was clicked
-  // forEach: remove the idea that's matcjng the unique od
-  // e.target ---> button that was clicked
-  var id = parseInt($(e.target).closest('article').attr('id'));
-
-  var ideas = this.getIdeas().filter(function(idea) {
-    return idea.id !== id;
-  });
-
-  // var ideas = this.getIdeas();
-  // var newIdeas = []
-  // ideas.forEach(function(idea) {
-  //     if(!idea.id === ideas.id) newIdeas.push(ideas);
-  // })
-
-  // extrac \t to a saveAndRender()
-
-  window.localStorage.setItem(this.storeKey, JSON.stringify(ideas));
-  render(ideaStore.getIdeas());
-}
-
-IdeaLocalStorage.prototype.updateIdea = function(e) {
-  // grab id of item that was clicked
-  // update title and body of idea that was clicked
-  // keep uniqueId
-  // update title & body of idea
-  var id = parseInt($(e.target).closest('article').attr('id'));
-  var ideaTitle = $(e.target).closest('.edit-title').text();
-  var ideaBody = $(e.target).closest('.edit-body').text();
-  // make an idea object using an Idea constructor
-  var idea = { id: id, title: ideaTitle, body: ideaBody };
-    ideaStore.addIdea(idea);
-
-    render(ideaStore.getIdeas());
-};
-//   var ideas = this.getIdeas();
-//   var newIdeas = [];
-//   ideas.forEach(function(existingIdea) {
-//       if(!existingIdea.uniqueId === idea.uniqueId) newIdeas.push(idea);
-//       else newIdeas.push(idea);
-//   });
-//   window.localStorage.setItem(this.storeKey, JSON.stringify(newIdeas));
-// }
-
-IdeaLocalStorage.prototype.search = function(searchText) {
-  var ideas = this.getIdeas();
-  var results = [];
-  ideas.forEach(function(idea) {
-      if(idea.title.indexOf(searchText) >= 0) results.push(idea);
-      else if (idea.body.indexOf(searchText) >= 0) results.push(idea);
-  });
-  return results;
-}
-
-var ideaStore = new IdeaLocalStorage();
 
 function createIdea() {
   var ideaTitle = $('.title-input-field').val();
@@ -90,7 +30,7 @@ function createIdea() {
 function render(ideas) {
   $('.idea-list-container').empty();
   ideas.forEach(function(idea) {
-    $('.idea-list-container').append(`
+    $('.idea-list-container').append (`
       <article id=${idea.id}>
       <span class="top line">
       <h2 contenteditable = "true" class="idea-edit edit-title">${idea.title}</h2>
@@ -106,10 +46,68 @@ function render(ideas) {
   });
 }
 
-// DELETE BUTTON
+
+IdeaLocalStorage.prototype.getIdeas = function() {
+  return JSON.parse(window.localStorage.getItem(this.storeKey)) || [];
+};
+
+IdeaLocalStorage.prototype.addIdea = function(idea) {
+  var ideas = this.getIdeas();
+  ideas.unshift(idea);
+  window.localStorage.setItem(this.storeKey, JSON.stringify(ideas));
+};
+
+IdeaLocalStorage.prototype.removeIdea = function(e) {
+  var newIdeas = [];
+  var id = parseInt($(e.target).closest('article').attr('id'));
+  var ideas = this.getIdeas().filter(function(idea) {
+    return idea.id !== id;
+  });
+  window.localStorage.setItem(this.storeKey, JSON.stringify(ideas));
+  render(ideaStore.getIdeas());
+};
+
+// +IdeaLocalStorage.prototype.removeIdea = function(uniqueId) {
+//   +  var ideas = this.getIdeas();
+//   +  var newIdeas = [];
+//   +  newIdeas.forEach(function(idea) {
+//     +      if(!idea.uniqueId === uniqueId) newIdeas.push(idea);
+//     +  })
+//     +  window.localStorage.setItem(this.storeKey, JSON.stringify(newIdeas));
+//   }
+
+IdeaLocalStorage.prototype.updateIdea = function(e) {
+  var ideas = this.getIdeas();
+  var newIdeas = [];
+  var id = parseInt($(e.target).closest('article').attr('id'));
+  var ideaTitle = $(e.target).closest('.edit-title').text();
+  var ideaBody = $(e.target).closest('.edit-body').text();
+  ideas.forEach(function(existingIdea) {
+    if(existingIdea.uniqueId !== idea.uniqueId) newIdeas.push(idea);
+    else newIdeas.push(idea);
+  });
+    window.localStorage.setItem(this.storeKey, JSON.stringify(newIdeas));
+};
+  // WHAT WAS WORKING FOR UPDATEIDEA
+//   var idea = { id: id, title: ideaTitle, body: ideaBody };
+//     ideaStore.addIdea(idea);
+//
+//     render(ideaStore.getIdeas());
+// };
+
+IdeaLocalStorage.prototype.search = function(searchText) {
+  var ideas = this.getIdeas();
+  var results = [];
+  ideas.forEach(function(idea) {
+      if(idea.title.indexOf(searchText) >= 0) results.push(idea);
+      else if (idea.body.indexOf(searchText) >= 0) results.push(idea);
+  });
+  return results;
+};
+
 $('.idea-list-container').on('click', '.delete-button', function(e) {
   ideaStore.removeIdea(e);
-})
+});
 
 $('.search-field').on('keyup', function() {
   var searchText = $(this).val();
@@ -117,17 +115,16 @@ $('.search-field').on('keyup', function() {
   render(searchResults);
 });
 
-$('.save-button').on('click', function() {
+$('.save-button').on('click', function(ideas) {
   createIdea();
   clearInputFields();
 });
 
 render(ideaStore.getIdeas());
 
-// SAVE ON RETURN PRESS
 $('.main-content').keypress(function(e) {
-    if(e.which == 13) {
-    ideaStore.updateIdea(e);
+  if(e.which == 13) {
+  ideaStore.updateIdea(e);
   }
 });
 
@@ -139,97 +136,3 @@ $('.main-content').keypress(function(e) {
 // $('.idea-edit').on('click', function(event) {
 //   event.isImmediatePropagationStopped();
 //   console.log('clickout');
-// });
-
-// DELETE BUTTON
-
-// EDITABLE FIELD FOR TITLE & BOVY
-
-// event handeler for each
-// triggers change to body/ title
-// saves and rerenders on event handeler (return or click out)
-
-
-// QUALITY
-
-// event handeler to trigger event
-// counter function up
-// counter function down
-// matching function with array
-
-//
-// // Singleton Object
-// var IdeaLocalStorage = {
-//   storeKey: 'ideas',
-//   ideas: [],
-//   removeIdea: function(id) {
-//       // refactoring to a singleton
-//   }
-// };
-
-
-// $('.idea-list-container').on('click', '#delete-article-button', function() {
-//   deleteIdea();
-// });
-
-// create objects around behaviour
-
-//run all logic and seperately from DOM and destroy and recreate DOM with every interaction
-
-// to clear everything (all stored data) set Store to empty string
-
-
-
-//
-// function deleteIdea() {
-//   $('#delete-article-button').closest('article').remove();
-// }
-//
-// function createIdea() {
-//   var $uniqueId = Date.now();
-//   var $ideaBody = $('.body-input-field').val();
-//   var $ideaTitle = $('.title-input-field').val();
-//   // var idea = new Idea($uniqueId, $ideaTitle, $ideaBody);
-//   $(`
-//     <article id=${$uniqueId} class="idea-article">
-//     <h2>${$ideaTitle}</h2>
-//     <button id="delete-article-button">DELETE</button>
-//     <p>${$ideaBody}</p>
-//     <button id="increase-quality-button">Thumbs Up</button>
-//     <button id="decrease-quality-button">Thumbs Down</button>
-//     </article>
-//     `).prependTo('.idea-list-container');
-// }
-//
-// function Idea(id, title, body) {
-//   this.id = id;
-//   this.title = title;
-//   this.body = body;
-// }
-
-// -//
-// -// function deleteIdea() {
-// -//   $('#delete-article-button').closest('article').remove();
-// -// }
-// -//
-// -// function createIdea() {
-// -//   var $uniqueId = Date.now();
-// -//   var $ideaBody = $('.body-input-field').val();
-// -//   var $ideaTitle = $('.title-input-field').val();
-// -//   // var idea = new Idea($uniqueId, $ideaTitle, $ideaBody);
-// -//   $(`
-// -//     <article id=${$uniqueId} class="idea-article">
-// -//     <h2>${$ideaTitle}</h2>
-// -//     <button id="delete-article-button">DELETE</button>
-// -//     <p>${$ideaBody}</p>
-// -//     <button id="increase-quality-button">Thumbs Up</button>
-// -//     <button id="decrease-quality-button">Thumbs Down</button>
-// -//     </article>
-// -//     `).prependTo('.idea-list-container');
-// -// }
-// -//
-// -// function Idea(id, title, body) {
-// -//   this.id = id;
-// -//   this.title = title;
-// -//   this.body = body;
-// -// }
